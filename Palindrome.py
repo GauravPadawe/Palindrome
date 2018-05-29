4018ebb1bfa3524147c29a42392f3ef970b1ebdc
Gaurav Padawe <grvpdw92@gmail.com>
2018-05-29 20:57:39 +0530

    Initial commit

---
+++ Palindrome.py
@@ -0,0 +1,13 @@
+﻿word = "coc"
+
+def palindrome(x):
+    y = x
+    while True:
+        x = x[::-1]
+        if x == y:
+            return 0
+            #print 0
+        else:
+            return -1
+            
+print palindrome(word)
